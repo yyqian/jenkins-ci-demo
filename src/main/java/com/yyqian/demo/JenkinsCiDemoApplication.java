898b6f74f236c50aa18767619936b423cf6d10db
Yinyin Qian <yyqian@live.com>
2016-04-15 17:04:27 +0800

    first commit

---
+++ src/main/java/com/yyqian/demo/JenkinsCiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.yyqian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCiDemoApplication.class, args);
+	}
+}
