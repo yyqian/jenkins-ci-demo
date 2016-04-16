c4147bc0dd952a3eed8c76ec0ad18bf10802b6e1
Yinyin Qian <yyqian@live.com>
2016-04-16 16:23:18 +0800

    test

--- src/main/java/com/yyqian/demo/controller/HelloController.java
+++ src/main/java/com/yyqian/demo/controller/HelloController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
   @RequestMapping("/")
   public String hello() {
-    return "Hello, world!";
+    return "Hello, world! Test 1";
   }
 }
